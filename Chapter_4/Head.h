5eaa1b2b47c2007e36ce6394bc1ddd8d6aa4748c
JoanYu2333 <yujoan2333@outlook.com>
2022-01-14 14:36:37 +0800

    2022/1/14

---
+++ Chapter_4/Head.h
@@ -0,0 +1,16 @@
+/**
+ * @File Name: Head.h
+ * @Brief : 
+ * @Author : YuJoan (yujoan2333@outlook.com)
+ * @Version : 1.0
+ * @Creat Date : 2022-01-12
+ * 
+ */
+
+#ifndef _HEAD_H
+#define _HEAD_H
+
+#include <iostream>
+
+#endif
+
---
+++ Chapter_4/Main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @File Name: Main.cpp
+ * @Brief : 
+ * @Author : YuJoan (yujoan2333@outlook.com)
+ * @Version : 1.0
+ * @Creat Date : 2022-01-12
+ * 
+ */
+
+#include "Head.h"
+
+int main()
+{
+    int a[5]{1, 2, 3, 4, 5};
+    printf("%d", a[2]);
+    system("pause");
+    return 0;
+}

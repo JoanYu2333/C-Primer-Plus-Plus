6827cd38cfdb1e31fadce29ee5e62017a1b57c3a
JoanYu2333 <yujoan2333@outlook.com>
2022-01-21 15:04:07 +0800

    2022/1/21

---
+++ Chapter_5/Head.h
@@ -0,0 +1,15 @@
+/**
+ * @File Name: Head.h
+ * @Brief : 
+ * @Author : YuJoan (yujoan2333@outlook.com)
+ * @Version : 1.0
+ * @Creat Date : 2022-01-21
+ * 
+ */
+
+#ifndef _HEAD_H_
+#define _HEAD_H_
+
+#include <iostream>
+
+#endif
---
+++ Chapter_5/Main.cpp
@@ -0,0 +1,16 @@
+/**
+ * @File Name: Main.cpp
+ * @Brief : 
+ * @Author : YuJoan (yujoan2333@outlook.com)
+ * @Version : 1.0
+ * @Creat Date : 2022-01-21
+ * 
+ */
+
+#include "Head.h"
+
+int main(void)
+{
+    system("pause");
+    return 0;
+}

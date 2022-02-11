73abdadd616835c217fb0c7e1eb8f5a323e91a4a
JoanYu2333 <yujoan2333@outlook.com>
2022-02-11 18:30:21 +0800

    2022/2/11

--- Chapter_5/Head.h
+++ Chapter_5/Head.h
@@ -16,4 +16,7 @@ void Example_1(void);
 void Example_2(void);
 void Example_3(void);
 void Example_4(void);
+void Example_5(void);
+void Example_6(void);
+void Example_7(void);
 #endif
--- Chapter_5/Main.cpp
+++ Chapter_5/Main.cpp
@@ -10,14 +10,8 @@
 #include "Head.h"
 int main(void)
 {
-    Example_4();
-    /*
-    int array[5] = {0, 1, 2, 3, 4};
-    for (int x : array)
-    {
-        std::cout << x << std::endl;
-    }
-    */
+    using namespace std;
+    Example_7();
     system("pause");
     return 0;
 }
@@ -81,4 +75,76 @@ void Example_4(void)
     cout << "The year is : " << count_year << endl;
     cout << "The money of Daphne and Cleo is : ";
     cout << money_Daphne << " " << money_Cleo;
+}
+
+void Example_5(void)
+{
+    using namespace std;
+    int number_sale_month[12];
+    int number_Sale_sum;
+    int i = 0;
+    string month[12] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
+    do
+    {
+        cout << "The number of " << month[i] << " are :";
+        cin >> number_sale_month[i];
+        number_Sale_sum += number_sale_month[i];
+    } while (i++ < 11);
+    cout << "The sum number are : " << number_Sale_sum;
+}
+
+void Example_6(void)
+{
+    using namespace std;
+    int number_sale_month[3][12];
+    int number_Sale_sum;
+    int year = 1;
+    int i = 0;
+    int j = 0;
+    string month[12] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
+    do
+    {
+        do
+        {
+            cout << "The number of " << j + 1 << " Year and " << month[i] << " Month is";
+            cin >> number_sale_month[j][i];
+            number_Sale_sum += number_sale_month[j][i];
+        } while (i++ < 11);
+        i = 0;
+    } while (j++ < 2);
+    cout << "The sum number are : " << number_Sale_sum;
+}
+
+void Example_7(void)
+{
+    using namespace std;
+    struct Car
+    {
+        string name;
+        int year_establish;
+    };
+    int number;
+    string rog;
+    int i = 0;
+    cout << "How many cars do you wish to catalog? ";
+    cin >> number;
+    cin.ignore();
+    Car *p = new Car[number];
+    while (i < number)
+    {
+        cout << "Car #" << i + 1 << endl;
+        cout << "Please enter the make : ";
+        getline(cin, (p + i)->name);
+        cout << "Please enter the year made : ";
+        cin >> (p + i)->year_establish;
+        cin.ignore();
+        i++;
+    }
+    i = 0;
+    cout << "Here is your collection" << endl;
+    while (i < number)
+    {
+        cout << (p + i)->year_establish << " " << (p + i)->name << endl;
+        i++;
+    }
 }
